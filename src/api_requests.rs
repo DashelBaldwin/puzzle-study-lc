5d1a2f8060e5afc55bde7f791c4b2a535d4e7f9c
DashelBaldwin <dashelbaldwin@gmail.com>
2024-10-23 16:03:47 -0500

    moved get_puzzles_from_ids and its helpers into new file get_puzzles_from_ids from main

--- src/api_requests.rs
+++ src/api_requests.rs
@@ -1,3 +1,4 @@
 // api_requests.rs
 
 pub mod json_objects;
+pub mod get_puzzles_from_ids;
--- src/main.rs
+++ src/main.rs
@@ -17,60 +17,11 @@ mod notation_utils;
 mod api_requests;
 
 use api_requests::json_objects::Puzzle;
-use api_requests::json_objects::DirectPuzzle;
-use api_requests::json_objects::DirectPuzzleGameData;
 use api_requests::json_objects::PuzzleAttempt;
-use api_requests::json_objects::parse_direct_puzzle;
 use api_requests::json_objects::parse_puzzle;
 
 const PAT: &str = "lip_4706DGPceC0b3H9YRO5x";
 const PAGE_SIZE: i32 = 50;
-
-async fn get_puzzle_from_id(id: &str) -> Result<Puzzle, Box<dyn Error>> {
-    let client = reqwest::Client::new();
-
-    let response = client
-        .get(format!("https://lichess.org/api/puzzle/{}", id))
-        .send()
-        .await?;
-
-    if response.status().is_success() {
-        let body = response.text().await?;
-        match parse_direct_puzzle(&body) {
-            Ok(direct_puzzle) => {
-                let puzzle = Puzzle {
-                    id: id.to_string(),
-                    rating: direct_puzzle.puzzle.rating,
-                    solution: direct_puzzle.puzzle.solution,
-                    themes: direct_puzzle.puzzle.themes,
-                    fen: notation_utils::pgn_to_fen::pgn_to_fen(&direct_puzzle.game.pgn)
-                };
-                println!("{}, {}", puzzle.fen, puzzle.id);
-                Ok(puzzle)
-            }
-            Err(e) => {
-                eprintln!("Failed to parse puzzle: {}", e);
-                Err(Box::from(e))
-            }
-        }
-
-    } else {
-        Err(Box::from(format!("API request error: {}", response.status())))
-    }
-
-}
-
-
-async fn get_puzzles_from_ids(ids: Vec<&str>) -> Result<Vec<Puzzle>, Box<dyn Error>> {
-    let mut puzzles: Vec<Puzzle> = Vec::new();
-
-    for id in ids {
-        puzzles.push(get_puzzle_from_id(id).await?);
-    }
-
-    Ok(puzzles)
-}
-
 
 async fn get_puzzle_history_incorrect_page(max: i32, before_date: i64) -> Result<(Vec<Puzzle>, i64), Box<dyn Error>> {
     let client = reqwest::Client::new();
